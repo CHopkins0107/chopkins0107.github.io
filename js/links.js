daa85e9539c7c6710e2f09e185654ea3b89e4c00
CHopkins0107 <chopkins0107@gmail.com>
2024-06-17 15:07:26 -0700

    Fixed path issues with javascript, changed css style elements, added icons to links

--- js/links.js
+++ js/links.js
@@ -2,11 +2,16 @@ export const links = [
     {
         name: "LinkedIn",
         link: "https://www.linkedin.com/in/jamelhopkins/",
-        image: "assets/linkedin.png",
+        image: "assets/In-White-48.png",
     },
     {
         name: "GitHub",
         link: "https://github.com/CHopkins0107",
         image: "assets/github-mark-white.png",
     },
+    {
+        name: "Email",
+        link: "mailto:chopkins0107@gmail.com",
+        image: "assets/email.png",
+    }
 ];
