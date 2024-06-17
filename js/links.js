9fa42df9604e95ad6821b032b178376894c09aad
CHopkins0107 <chopkins0107@gmail.com>
2024-06-17 13:48:32 -0700

    Initial addition of css and javascript, testing links

---
+++ js/links.js
@@ -0,0 +1,12 @@
+export const links = [
+    {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/jamelhopkins/",
+        image: "assets/linkedin.png",
+    },
+    {
+        name: "GitHub",
+        link: "https://github.com/CHopkins0107",
+        image: "assets/github-mark-white.png",
+    },
+];
